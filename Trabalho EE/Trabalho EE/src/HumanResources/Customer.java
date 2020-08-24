f6704139b53be6cc8b4909ed5f73bdafce417c82
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-08-24 19:06:50 +0100

    start of person and customer

---
+++ Trabalho EE/Trabalho EE/src/HumanResources/Customer.java
@@ -0,0 +1,37 @@
+package HumanResources;
+import hr.IAddress;
+import hr.ICustomer;
+import java.time.LocalDate;
+
+/*
+* Nome: <Samuel Luciano Correia da Cunha>
+* Número: <8160526>
+*/
+public class Customer implements ICustomer {
+
+    @Override
+    public String getVat() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IAddress getAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public LocalDate getBirthDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Trabalho EE/Trabalho EE/src/HumanResources/Person.java
@@ -0,0 +1,32 @@
+package HumanResources;
+import hr.IAddress;
+import hr.IPerson;
+import java.time.LocalDate;
+
+/*
+* Nome: <Samuel Luciano Correia da Cunha>
+* Número: <8160526>
+*/
+public class Person implements IPerson {
+
+    @Override
+    public String getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IAddress getAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public LocalDate getBirthDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
