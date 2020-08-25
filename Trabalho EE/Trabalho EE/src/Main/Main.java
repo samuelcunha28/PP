4d37227fd6d06eda3547c099de3648b428ed3929
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-08-25 17:37:55 +0100

    comments on main fixed

--- Trabalho EE/Trabalho EE/src/Main/Main.java
+++ Trabalho EE/Trabalho EE/src/Main/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
 import HumanResources.Address;
@@ -10,10 +5,10 @@ import HumanResources.Customer;
 import java.time.LocalDate;
 import java.time.Month;
 
-/**
- *
- * @author samue
- */
+/*
+* Nome: <Samuel Luciano Correia da Cunha>
+* Número: <8160526>
+*/
 public class Main {
 
     /**
