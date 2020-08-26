91b2f67605e74b49eddd820e854e00914e150cf7
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-08-26 14:52:00 +0100

    driver done and hr all done with successfull results on main testing

--- Trabalho EE/Trabalho EE/src/HumanResources/Driver.java
+++ Trabalho EE/Trabalho EE/src/HumanResources/Driver.java
@@ -5,61 +5,179 @@ import hr.IDriver;
 import hr.LicenseType;
 import java.time.LocalDate;
 import transport.DriverStatus;
+import Exceptions.*;
+import java.util.Arrays;
 
 /*
 * Nome: <Samuel Luciano Correia da Cunha>
 * Número: <8160526>
 */
-public class Driver implements IDriver {
+public class Driver extends Person implements IDriver {
 
+    /**
+     * The driver starting position date.
+     */
+    private LocalDate startingPosition;
+    
+    /**
+     * The driver status (default = free).
+     */
+    private DriverStatus status = DriverStatus.FREE;
+    
+    /**
+     * The number of licenses.
+     */
+    private int numberOfLicenses;
+    
+    /**
+     * The driver LicenseType.
+     */
+    private LicenseType[] licenses;
+    
+    public Driver(String id, IAddress address, String name, LocalDate birthDate, LocalDate startingPosition, DriverStatus status) {
+        super(id, address, name, birthDate);
+        this.startingPosition = startingPosition;
+        this.status = status;
+        this.numberOfLicenses = 0;
+        this.licenses= new LicenseType[5];
+    }
+
+    /**
+     * Getter for starting position date.
+     * @return The starting position date.
+     */
     @Override
     public LocalDate getStartingPositionDate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return startingPosition;
     }
 
+    /**
+     * Adds a license to the driver.
+     * @param lt The license type to be added.
+     * @return True if inserted, false otherwise.
+     * @throws HRException if licenseType is null && DriverStatus is different from free.
+     */
     @Override
     public boolean addLicense(LicenseType lt) throws HRException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.status != DriverStatus.FREE) {
+            throw new HRExceptionImpl("The driver status is assigned");
+        }
+        if (lt == null) {
+            throw new HRExceptionImpl("The driver license is null");
+        }
+
+        if (this.licenses.length == this.numberOfLicenses) {
+            LicenseType[] clone = this.licenses;
+            this.licenses = new LicenseType[this.licenses.length + 1];
+            for (int i = 0; i < clone.length; i++) {
+                this.licenses[i] = clone[i];
+            }
+        }
+
+        this.licenses[this.numberOfLicenses] = lt;
+        this.numberOfLicenses++;
+
+        return true;
     }
 
+    /**
+     * Removes a license from the driver.
+     * @param lt The license type to be removed.
+     * @return True if removed, false otherwise.
+     * @throws HRException if licenseType is null && DriverStatus is different from free.
+     */
     @Override
     public boolean removeLicense(LicenseType lt) throws HRException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.status != DriverStatus.FREE) {
+            throw new HRExceptionImpl("The driver status is assigned");
+        }
+        if (lt == null) {
+            throw new HRExceptionImpl("The driver license is null");
+        }
+
+        for (int i = 0; i < this.numberOfLicenses; ++i) {
+            if (this.licenses[i].equals(lt)) {
+                for (; i < this.numberOfLicenses - 1; i++) {
+                    this.licenses[i] = this.licenses[i + 1];
+                }
+                this.licenses[i] = null;
+                this.numberOfLicenses--;
+                return true;
+            }
+        }
+        return false;
     }
 
+    /**
+     * Checks if the driver has a specific license type.
+     * @param lt The license type to be checked.
+     * @return True if exists, false otherwise.
+     */
     @Override
     public boolean haveLicense(LicenseType lt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int i;
+        if (numberOfLicenses == 0) {
+            return false;
+        }
+
+        for (i = 0; i < this.numberOfLicenses; ++i) {
+            if (lt.equals(this.licenses[i])) {
+                return true;
+                
+            }
+        }
+        return false;
     }
 
+    /**
+     * Getter for driver status.
+     * @return The driver status.
+     */
     @Override
     public DriverStatus getStatus() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return status;
     }
 
+    /**
+     * Setter for driver status.
+     * @param ds The driver status.
+     */
     @Override
     public void setStatus(DriverStatus ds) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.status = ds;
     }
 
+    /**
+     * To String method representing the Driver.
+     * @return String with all the information about the driver.
+     */
     @Override
-    public String getId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String toString() {
+        return super.toString() + "\nStartingPosition: " + startingPosition + 
+                "\nStatus: " + status + "\nNumber of Licenses: " + numberOfLicenses + 
+                "\nLicenses: " + Arrays.toString(licenses);
     }
 
+    /**
+     * Compares two Driving licenses.
+     * @param obj object to be compared
+     * @return true if obj is equal to the driver license type.
+     */
     @Override
-    public IAddress getAddress() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public LocalDate getBirthDate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Driver other = (Driver) obj;
+        if (!Arrays.deepEquals(this.licenses, other.licenses)) {
+            return false;
+        }
+        return true;
+    } 
 }
--- Trabalho EE/Trabalho EE/src/Main/Main.java
+++ Trabalho EE/Trabalho EE/src/Main/Main.java
@@ -2,8 +2,12 @@ package Main;
 
 import HumanResources.Address;
 import HumanResources.Customer;
+import HumanResources.Driver;
+import exceptions.HRException;
+import hr.LicenseType;
 import java.time.LocalDate;
 import java.time.Month;
+import transport.DriverStatus;
 
 /*
 * Nome: <Samuel Luciano Correia da Cunha>
@@ -13,12 +17,22 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws exceptions.HRException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws HRException {
         Address address1 = new Address("Porto", "Porto", 1, "Porto", "Portugal");
         Customer customer1 = new Customer("1234", "123", address1, "samuel", LocalDate.of(1998, Month.NOVEMBER, 12));
+        Driver driver1 = new Driver("1", address1, "samuel", LocalDate.of(1998, Month.NOVEMBER, 12), LocalDate.of(2019, Month.NOVEMBER, 12), DriverStatus.FREE);
         
-        System.out.println(customer1.getVat());
-        System.out.println(customer1.getName());
+        // System.out.println(customer1.getVat());
+        // System.out.println(customer1.getName());
+        driver1.addLicense(LicenseType.A1);
+        driver1.addLicense(LicenseType.A2);
+        System.out.println(driver1);
+        System.out.println("");
+        driver1.removeLicense(LicenseType.A1);
+        System.out.println(driver1);
+        System.out.println("");
+        System.out.println(driver1.haveLicense(LicenseType.A2));
     }
 }
