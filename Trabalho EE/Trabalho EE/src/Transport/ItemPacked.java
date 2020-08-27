3c8204658f758a0bb687da380f9797e536912d24
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-08-27 19:55:14 +0100

    Item is done, next up is ItemPacked, Vehicle and Truck

---
+++ Trabalho EE/Trabalho EE/src/Transport/ItemPacked.java
@@ -0,0 +1,27 @@
+package Transport;
+import transport.IItem;
+import transport.IItemPacked;
+import transport.IPosition;
+
+/*
+* Nome: <Samuel Luciano Correia da Cunha>
+* Número: <8160526>
+*/
+public class ItemPacked implements IItemPacked {
+
+    @Override
+    public IItem getItem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IPosition getPosition() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setPosition(IPosition ip) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
